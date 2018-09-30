5b8c32d636e04c3c7740361d28bd469708936cfc
katongole <katsisaac50@gmail.com>
2018-09-30 06:11:48 +0200

    Heading transferred into a reusable component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import WeatherSearch from './weatherSearch';
+import Headers from './component/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>weatherapp</h1>
-        <WeatherSearch/>
+      <Headers/>
+      <WeatherSearch/>
       </div>
     );
   }

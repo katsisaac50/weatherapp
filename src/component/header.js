d7b432141f9781dd3d392313b53c46e044ec3cca
katongole <katsisaac50@gmail.com>
2018-09-30 06:16:33 +0200

    header component created

---
+++ src/component/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Headers extends Component {
+  render() {
+    return (
+      <header className='header'>
+      <h1>Weather App</h1>
+      </header>
+    )
+  }
+}
+
+export default Headers
